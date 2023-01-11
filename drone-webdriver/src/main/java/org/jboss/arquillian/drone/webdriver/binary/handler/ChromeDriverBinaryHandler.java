40edd5fc3020b71e3a9e0b70821003eb0be5e3cf
Michael Stringer <stringbean@users.noreply.github.com>
2023-01-11 12:30:46 +0000

    Fetch ARM ChromeDriver on Apple Silicon

--- drone-webdriver/src/main/java/org/jboss/arquillian/drone/webdriver/binary/handler/ChromeDriverBinaryHandler.java
+++ drone-webdriver/src/main/java/org/jboss/arquillian/drone/webdriver/binary/handler/ChromeDriverBinaryHandler.java
@@ -116,6 +116,9 @@ public class ChromeDriverBinaryHandler extends AbstractBinaryHandler {
             final StringBuilder fileName = new StringBuilder("chromedriver_");
             if (PlatformUtils.isMac()) {
                 fileName.append("mac");
+                if (PlatformUtils.isMacAppleSilicon()) {
+                    fileName.append("_arm");
+                }
             } else if (PlatformUtils.isWindows()) {
                 fileName.append("win");
             } else if (PlatformUtils.isUnix()) {
